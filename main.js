485236b3ddf000016ca565482fbf8a4df1a854c1
ilanaoliveira <128821749+ilanaoliveira@users.noreply.github.com>
2023-10-29 16:21:49 -0300

    criando função

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-document.querySelector('.tecla_groot');
+function tocaSomGroot () {
+    document.querySelector('#som_tecla_groot').play();
+}
 
-document.querySelector('#som_tecla_groot').play();
+
+document.querySelector('.tecla_groot');
